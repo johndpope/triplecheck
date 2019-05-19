6506ad9977ef487082e0e6d9f0bf6816f371602a
Harish Prasanna <harishperfect@gmail.com>
2019-05-19 09:39:49 -0400

    added cors

--- app/core/routes.py
+++ app/core/routes.py
@@ -174,3 +174,15 @@ def listefiles():
     data = dumps(mongo.db.encrypted.find())
     return data
 
+
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'OPTIONS, DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+
+app.after_request(add_cors_headers)
+
